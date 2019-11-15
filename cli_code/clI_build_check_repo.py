54afae394924996007a973f98dc30b15c2728bcf
gumnn <arita37@users.noreply.github.com>
2019-11-15 15:03:07 +0900

    Create clI_build_check_repo.py

---
+++ cli_code/clI_build_check_repo.py
@@ -0,0 +1,60 @@
+
+"""
+
+Create python script to do this :
+
+For a given github repo :
+
+   1)   git clone
+   2)  Build conda env with packages
+   3)  check if python code is running
+            main.py
+  4) Generate signature docs of python source code
+
+
+Goal is to automate code check using a python script
+
+
+
+""""
+
+
+
+
+#    1)   git clone repoURL
+
+
+
+
+
+
+# 2)  Build conda env from source code parsing
+
+
+
+# 3) Launch all files in root (main.py) with subprocess to check if running
+     Log error messages on disk
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+# 4) Parse source code with inspect and generate signature for each functiom
+    Write on disk
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
